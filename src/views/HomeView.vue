9cd0d31f794cc35e134cfccfdb625650e8d069b3
Kevin <kayaswp@gmail.com>
2022-03-28 20:48:17 +0630

    using ref for Reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,23 @@
 <template>
   <h1>Home Page</h1>
-  <p>Hello! I'm {{name}} and {{age}} years old.</p>
+  <!-- not need .value if we use datas outside -->
+  <p>Hello! I'm {{name}} and {{age}} years old.</p> 
+  <input type="text" v-model="name">
   <button @click="handleClick">Click</button>
+  <button @click="age++">Increase age</button>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 export default {
   setup(){
-    let name = "KayAs"
-    let age = 23
+    let name = ref("KayAs")
+    //console.log("name.value")
+    let age = ref(23)
     let handleClick=()=>{
-      console.log('Click function')
+      name.value = "Kevin Phon" //remember to use .value
     }
-    return {name,age,handleClick}//remember to return datas in composition api. 
-                      // so that we can use them
+    return {name,age,handleClick}
   }
 }
 </script>
