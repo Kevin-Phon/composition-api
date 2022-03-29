7fa68234b542bd2cf5a857ed97a57c56ee9d75a7
Kevin <kayaswp@gmail.com>
2022-03-29 19:53:02 +0630

    ref vs reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,33 @@
 <template>
-  <h1>Home Page</h1>
-  <!-- not need .value if we use datas outside -->
-  <p>Hello! I'm {{name}} and {{age}} years old.</p> 
-  <input type="text" v-model="name">
-  <button @click="handleClick">Click</button>
-  <button @click="age++">Increase age</button>
+  <h1>Reactive vs ref</h1>
+  <p>User one - {{user1}}</p>
+  <p>User two - {{user2}}</p>
+  <p>Person two - {{person2.name}}</p>
+  <button @click="changeOne">User1 button</button>
+  <button @click="changeTwo">User2 button</button>
+  <button @click="personTwo">Person2 button</button>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import { reactive, ref } from '@vue/reactivity'
 export default {
   setup(){
-    let name = ref("KayAs")
-    //console.log("name.value")
-    let age = ref(23)
-    let handleClick=()=>{
-      name.value = "Kevin Phon" //remember to use .value
+    let user1 = ref("Kevin")
+    let user2 = reactive("Phon")
+
+    let person2 = reactive({name:"KS"})
+
+    let changeOne=()=>{
+      user1.value = "KayAs"
     }
-    return {name,age,handleClick}
+    let changeTwo=()=>{
+      user2 = "WP" // in reactive dose not need .value but only in object and array
+    }
+    let personTwo=()=>{
+      person2.name = "wp" //not need .value because its object
+    }
+
+    return {user1,user2,changeOne,changeTwo,person2,personTwo}
   }
 }
 </script>
