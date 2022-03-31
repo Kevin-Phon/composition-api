89aa47246a34e172b44628914bfb3ea436c28420
Kevin <kayaswp@gmail.com>
2022-03-31 14:31:30 +0630

    using life cycle hook in composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
-  <PostsList :posts="posts"></PostsList>
-  
+  <div v-if="showPost"> 
+    <PostsList :posts="posts"></PostsList>
+  </div>
+  <button @click="showPost=!showPost">Toggle Post</button>
+  <button @click="posts.pop()">Delete</button>
 </template>
 
 <script>
@@ -10,12 +13,15 @@ import { ref } from '@vue/reactivity'
 export default {
   components: { PostsList },
   setup(){
+    let showPost = ref(true)
     let posts = ref([
       {title:"post title 1",body:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores a numquam dolore itaque est tempora, deserunt, nam earum aperiam dolor deleniti error perferendis quod. Nostrum quod eos velit ea laudantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, dolorum. Soluta at suscipit illum quos, delectus in nobis dolores fuga tempora sapiente! Adipisci quaerat, eveniet placeat consequatur qui rerum dicta.",id:1},
-      {title:"post title 2",body:"lorem ipsum",id:2}
+      {title:"post title 2",body:"lorem ipsum",id:2},
+      {title:"post title 3",body:"lorem ipsum",id:3},
+      {title:"post title 4",body:"lorem ipsum",id:4}
     ])
 
-    return {posts}
+    return {posts,showPost}
   }
 }
 </script>
