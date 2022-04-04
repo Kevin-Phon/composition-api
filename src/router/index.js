6130fd64b0d0a4268b7fc011fad86803b95a7545
Kevin <kayaswp@gmail.com>
2022-04-04 20:17:02 +0630

    create form  frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DetailView from '../views/DetailView.vue'
+import CreateView from '../views/CreateView.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'Detail',
     component: DetailView,
     props: true
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: CreateView
   }
 ]
 
